368cd95f27aae4261d7a5184ef7ec6432e15f971
emmabrnnr <39092684+emmabrnnr@users.noreply.github.com>
2020-01-01 15:18:58 +0100

    début interface graphique

--- RobotTurtles/src/com/company/Main.java
+++ RobotTurtles/src/com/company/Main.java
@@ -1,12 +1,53 @@
 package com.company;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class Main {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Main extends StateBasedGame
+{
+
+    public Main(String name)
+    {
+        super(name);
+    }
+
+    public void initStatesList(GameContainer gc) throws SlickException
+    {
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         Menu menu = new Menu();
         menu.display();
+        JFrame f = new JFrame("Robot Turtles");
+        f.setSize(3000,1000);
+        JPanel plateau = new JPanel();
+        plateau.setLocation(100, 100);
+        plateau.setSize(100,78);
+        plateau.setBackground(Color.CYAN);
+        f.add(plateau,BorderLayout.CENTER);
 
+        try {
+            f.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("C://Users//emmab//Downloads//RobotTurtles-master//RobotTurtles-master//images//images//background.jpeg")))));
+       } catch (IOException e) {
+            e.printStackTrace();
+        }
+        f.pack();
+        f.setVisible(true);
+        plateau.setVisible(true);
     }
-}
+
+
+
+    Main jeu = new Main("Robot turtles");
+    }
+
